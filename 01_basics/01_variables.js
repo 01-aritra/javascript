d9e86a3f40f67dd2f731ba8c8da2c9c08bdcf80e
artira <aritramishra64@gmail.com>
2026-05-27 14:24:22 +0530

    initial change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+//Declaring a variable in js
+const accountID = 1;
+let accountName = "Aritra";
+let accountEmailId = "aritramishra64@gmail.com" 
+var accountPassword = "12345"
+
+accountID = 2
+
+
+console.log();
